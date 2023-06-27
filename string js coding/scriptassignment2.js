675aa809ac0cfb10e1d72ff4e3835a88e9849ed1
shikhabisen01 <127319064+shikhabisen01@users.noreply.github.com>
2023-06-28 00:29:07 +0530

    Add files via upload

---
+++ string js coding/scriptassignment2.js
@@ -0,0 +1,26 @@
+
+
+const str = 'loveleetcode';
+const example2 = (str = '') => {
+   let obj = {};
+   for(let i = 0; i < str.length; i++){
+      if(str[i] in obj){
+         let temp = obj[str[i]];
+         let x = parseInt(temp[0]);
+         x += 1;
+         temp[0] = x;
+         obj[str[i]] = temp;
+      } else {
+         obj[str[i]] = [1, i]
+      }
+   }
+   let arr = Object.keys(obj);
+   for(let i = 0; i < arr.length; i++){
+      let z = obj[arr[i]]
+      if(z[0] === 1){
+         return z[1];
+      }
+   }
+   return -1;
+};
+console.log(example2(str));
